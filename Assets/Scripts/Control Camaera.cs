0f58690aac0cd325581097aea0c1a26af38b0e87
causapefx <lugui1922@gmail.com>
2016-09-11 22:19:21 +0200

    Testing Scene with Script
    v.0.0.1 / 11-09-2016
    Causape
    
    -Included a test scene.
    -Included a plataform to the movement of the player.
    -Included a script for the movement of the camera.

---
+++ Assets/Scripts/Control Camaera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public GameObject Player;
+	// Update is called once per frame
+	void Update () {
+		//All that is here, it begins one time for frame.
+		//x = y + distancia.
+		transform.position = new Vector3(Player.transform.position.x + 5,transform.position.y, transform.position.z);
+	}
+}
