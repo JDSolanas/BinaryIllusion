bb486d6317d790f7ed42f67395d7ea68d503c8d4
causapefx <lugui1922@gmail.com>
2016-09-11 22:33:45 +0200

    Fixed error with Script
    v.0.0.1 / 11--9-2016
    Causape

--- Assets/Scripts/ControlCamera.cs
+++ Assets/Scripts/ControlCamera.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ControlCamera : MonoBehaviour {
 
 	public GameObject Player;
 	// Update is called once per frame
 	void Update () {
 		//All that is here, it begins one time for frame.
-		//x = y + distancia.
 		transform.position = new Vector3(Player.transform.position.x + 5,transform.position.y, transform.position.z);
 	}
 }
